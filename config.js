f106df86db88e614ccc8fa4d5c96ec81a1e8ac5c
HarryPotter-coder79 <osayamonharrypotter@gmail.com>
2026-02-21 21:27:16 +0100

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    OWNER_JIDS: ['2349051579932@s.whatsapp.net'],
+    SUDO_JIDS: ['2349051579932@s.whatsapp.net'],
+    PREFIXES: ['.'],
+    ANTI_DELETE: true,
+    AUTO_READ: true,
+    AUTO_STATUS: true
+};
