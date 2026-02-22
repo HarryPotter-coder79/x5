ccbdf7be764dc1b58c5cd49f756a2acebcd6e827
HarryPotter-coder79 <osayamonharrypotter@gmail.com>
2026-02-22 09:40:42 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// v2
-module.exports = {
-    OWNER_JIDS: ['2349051579932@s.whatsapp.net'],
-    SUDO_JIDS: ['2349051579932@s.whatsapp.net'],
-    PREFIXES: ['.'],
-    ANTI_DELETE: true,
-    AUTO_READ: true,
-    AUTO_STATUS: true
-};
