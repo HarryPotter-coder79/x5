28fdcb5e8bf2cd67d5e3dc01e9cd326a7d69affc
HarryPotter-coder79 <osayamonharrypotter@gmail.com>
2026-02-22 09:41:21 +0100

    Add configuration for bot settings

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botname: 'X5 Bot',
+    ownerNumber: '2349051579932',
+    ownerNumber: '2349031575131',
+    prefix: '.',
+    author: 'Harry Potter',
+    packname: 'WhatsApp Bot'
+};
